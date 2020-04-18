aee567ad67c6dbfc288fd76f2d68a0532d097204
Mac <mac.kittipat@gmail.com>
2020-04-18 17:25:27 +0700

    Implement lombok log sample

---
+++ mac-javalog/src/main/java/com/mackittipat/App.java
@@ -0,0 +1,12 @@
+package com.mackittipat;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    public static void main( String[] args ) {
+        log.debug("Hello World");
+        log.info("Hello Java");
+    }
+}
